14e4b8b11316cf6d3ba32f2a47a08f6e572ec79a
dominic-myb <desguerra246@gmail.com>
2024-02-10 19:11:39 +0800

    Fix Enemy Chasing issue

--- Assets/Scripts/EnemyScript/EnemyChase.cs
+++ Assets/Scripts/EnemyScript/EnemyChase.cs
@@ -11,64 +11,70 @@ public class EnemyChase : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private GameObject player;
     private Rigidbody2D rb;
-    private float speed = .5f; 
-    public float distanceBetween = 2f; 
+    private readonly float speed = 1f;
+    public float distanceBetween = 2f;
     public float distance;
-    private float previousX; 
+    private float previousX;
     private bool hasLineOfSight = false;
-    private void Start() {
-        player = GameObject.FindGameObjectWithTag("Player"); 
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
-    private void Update() {
-        if(distance < distanceBetween){
-            EnemyDirection();
-            StartMove();
-            // if(hasLineOfSight){
-                transform.position = UnityEngine.Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime); 
-            // }
-        }else{
-            StopMove();
+    private void Update()
+    {
+        if (distance < distanceBetween && hasLineOfSight)
+        {
+            StartFollow();
+            transform.position = UnityEngine.Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
-        
+        else
+        {
+            StopFollow();
+        }
     }
-    private void FixedUpdate() {
-        distance = UnityEngine.Vector2.Distance(transform.position, player.transform.position); 
-        UnityEngine.Vector2 direction = player.transform.position - transform.position; 
-    
-        // RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction);
-        // foreach (RaycastHit2D hit in hits){
-        //     if(hit.collider!=null){
-        //         if(hit.collider.CompareTag("Player")){
-        //             hasLineOfSight = true;
-        //         }else{
-        //             hasLineOfSight = false;
-        //         }
-        //     }
-        // }
-        
-        Debug.DrawRay(transform.position, player.transform.position - transform.position, hasLineOfSight ? Color.green : Color.red);
+    private void FixedUpdate()
+    {
+        distance = UnityEngine.Vector2.Distance(transform.position, player.transform.position);
+        UnityEngine.Vector2 direction = player.transform.position - transform.position;
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction.normalized, distance);
+        bool hitBlock = false;
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Block"))
+                {
+                    hitBlock = true;
+                    break;
+                }
+            }
+        }
+        hasLineOfSight = !hitBlock;
+
+        if (distance < distanceBetween && hasLineOfSight)
+        {
+            Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+        }
     }
-    private void EnemyDirection(){
-        float currentX = transform.position.x; 
-        if(currentX > previousX) spriteRenderer.flipX = false;
-        else if(currentX < previousX) spriteRenderer.flipX = true;
+    private void StartFollow()
+    {
+        float currentX = transform.position.x;
+        if (currentX > previousX) spriteRenderer.flipX = false;
+        else if (currentX < previousX) spriteRenderer.flipX = true;
         previousX = currentX;
+        animator.SetBool("isMoving", true);
     }
-    private bool InRange(){
-        if(distance < distanceBetween/2){
-            return true;
-        }else{
-            return false;
-        }
-        
-    }
-    private void StartMove(){
-        animator.SetBool("isMoving",true);
-    }
-    private void StopMove(){
-        animator.SetBool("isMoving",false);
+    private void StopFollow()
+    {
+        animator.SetBool("isMoving", false);
     }
 }
